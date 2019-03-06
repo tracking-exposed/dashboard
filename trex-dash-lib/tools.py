cdef141b9149aadbdc88e135f58d8220dd9696d3
berli0z <40333748+berli0z@users.noreply.github.com>
2019-03-06 19:45:35 +0100

    Update tools.py

--- trex-dash-lib/tools.py
+++ trex-dash-lib/tools.py
@@ -17,13 +17,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 # load configuration
-p = configargparse.ArgParser(
-    # default_config_files=[os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "config/general.conf"))]
-)
-p.add('-n', '--name', help='arbitrary name for profile', required=True)
-p.add('-c', '--config', is_config_file=True, help='config file path')
-p.add('-i', '--id', help='id of your fbtrex user', required=True)
-config = vars(p.parse_args())
+from config import config
 
 # reads pandas dataframe from getSummary output
 # returns list of unique timeline ids for the dataset
