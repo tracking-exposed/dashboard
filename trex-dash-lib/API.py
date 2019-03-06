9d33be5ca531096baf28e40a301e20c2937114c4
berli0z <40333748+berli0z@users.noreply.github.com>
2019-03-06 19:18:17 +0100

    Update API.py

--- trex-dash-lib/API.py
+++ trex-dash-lib/API.py
@@ -1,7 +1,10 @@
 from cachetools import cached, TTLCache
 import requests
+import pandas as pd
+
 
 cache = TTLCache(maxsize=1, ttl=600) # time to live is 10 mins (except its stored = problem might be here)
+
 
 # calls api summary and returns a pandas dataframe. this could be generic when APIs are final
 @cached(cache)
@@ -9,4 +12,5 @@ def getSummary(id):
     url = 'https://facebook.tracking.exposed/api/v1/summary/' + str(id) + '/' + '0'
     print("Accessing summary, ID hidden.")
     data = requests.get(url).json()
-    return data
+    df = pd.DataFrame.from_records(data)
+    return df
