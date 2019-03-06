ca23640c01bb260e538d04801ff29ff339b1e996
berli0z <40333748+berli0z@users.noreply.github.com>
2019-03-06 19:41:05 +0100

    Update tester.py

--- test/tester.py
+++ test/tester.py
@@ -1,1 +1,15 @@
+import sys, os
+script_dir = os.path.dirname(os.path.dirname(__file__)) #<-- absolute dir the dir of the script is in
+rel_path = "trex-dash-lib"
+sys.path.append(os.path.join(script_dir, rel_path))
+import tools
 
+#this while is used to keep cache alive until the problem is found with pickle
+while True:
+    cmd = input('Please say run or exit: _')
+    if cmd == 'run':
+        tools.saveDailyImpressions(tools.config)
+    elif cmd == 'exit':
+        exit()
+    else:
+        print('incorrect command, try again')
