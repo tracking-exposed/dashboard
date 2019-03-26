91f1d2636b9713f47f3318208d11d043b37b1679
berli0z <40333748+berli0z@users.noreply.github.com>
2019-03-26 17:05:43 +0100

    Update API.py
    implementing persistent transparent caching (using sqlite3)

--- lib/API.py
+++ lib/API.py
@@ -1,14 +1,60 @@
-from cachetools import cached, TTLCache
 import requests
 import pandas as pd
+import requests_cache
+import os
 
-cache = TTLCache(maxsize=1, ttl=600) # 600 time to live is 10 mins (except its stored = problem might be here)
+# absolute path
+script_dir = os.path.dirname(os.path.dirname(__file__))  # <-- absolute dir the script is in
+rel_path = "./local/api-cache"
+strCache = os.path.join(script_dir, rel_path)
+
+# Initialize Caching
+requests_cache.install_cache(backend='sqlite', expire_after=600, cache_name=strCache)
+
+# Custom errors handling
+class EmptyDataframeError(Exception):
+    pass
+
+def checkId(id):
+    if (len(id) != 40):
+        raise ValueError('the ID in configuration should be 40 characters long. This is '+str(len(id)))
+        return
+    else:
+        return
+
+def checkData(data):
+    if (data.headers['Content-Type'] != 'application/json; charset=utf-8'):
+        raise RuntimeError('Dataframe error')
+        return
+    else:
+        return
+
+def checkDf(df):
+    if df.empty:
+        raise EmptyDataframeError()
+    elif type(df) == 'NoneType':
+        raise EmptyDataframeError()
+    else:
+        return
 
 # calls api summary and returns a pandas dataframe. this could be generic when APIs are final
-@cached(cache)
 def getSummary(id):
+
+    #check that id is correct
+    checkId(id)
+
+    # setup HTTP request
     url = 'https://facebook.tracking.exposed/api/v1/summary/' + str(id) + '/' + '0'
     print("Accessing summary, ID hidden.")
-    data = requests.get(url).json()
-    df = pd.DataFrame.from_records(data)
+
+    # call API
+    data = requests.get(url)
+
+    # Check that Dataframe is not empty
+    checkData(data)
+
+    # convert to DataFrame
+    df = pd.DataFrame.from_records(data.json())
+
+    checkDf(df)
     return df
