fda625b04c2f3342ba779bfafad5c8f07d3c575a
Claudio Agosti <claudio@tracking.exposed>
2019-04-02 14:45:58 +0200

    wip to align our dev env

--- bin/tryme.py
+++ bin/tryme.py
@@ -1,6 +1,14 @@
-from lib import API, tools
 
-id = ''
+import os
+import sys
+script_dir = os.path.dirname(os.path.dirname(__file__)) #<-- absolute dir the dir of the script is in
+rel_path = "lib"
+sys.path.append(os.path.join(script_dir, rel_path))
+import API, tools, viz
+
+# from lib import API, tools
+
+id = 'af4b578f432046bb5569e024d48319419652bfc0';
 df =  API.getDF(id, 'summary', 20000, 0)
 df = tools.setDatetimeIndex(df, 'impression')
 df = tools.setDatetimeIndexFloor(df, '1H')
