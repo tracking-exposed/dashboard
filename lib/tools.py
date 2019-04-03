2d392558cd51cbdd11375109b17ec9f961eb4824
berli0z <40333748+berli0z@users.noreply.github.com>
2019-04-03 10:06:43 +0200

    Update tools.py

--- lib/tools.py
+++ lib/tools.py
@@ -1,15 +1,9 @@
-import pandas as pd
-from datetime import datetime
-import os
-# register converters
-from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
+import os ; import pandas as pd ; from pandas.plotting import register_matplotlib_converters ; register_matplotlib_converters() # register converters
+from lib.config import config
 
-# load configuration
-# from config import config
+'''Takes a column, removes all the duplicates
+    and returns a list of unique values.'''
 
-# reads pandas dataframe from getSummary output
-# returns list of unique timeline ids for the dataset
 def makeList(df, what):
     newList = []
     for index, row in df.iterrows():
@@ -20,31 +14,11 @@ def makeList(df, what):
             pass
     return newList
 
-# reads pandas dataframe and a list of timeline ids (output from function getTimelineList)
-# returns a dataframe with datetimeindex and impressionCount
-# NOTE: THIS IS INTENDED TO BE USED ONLY WITH PROFILES THAT ARE MAKING USE OF THE AUTOSCROLLING TAMPERMONKEY SCRIPT.
-def doImpressionCount(df, timelinelist):
-    maxlist = [] # boot lists
-    dateslist = [] #
-    df2 = pd.DataFrame()
-    for timeline in timelinelist: # we group all the impressions together for each timeline id
-        df2 = df[df['timeline'] == timeline]  #create a new dataset filtered for a specific timeline id
-        maxim = df2.impressionOrder.values.max() # the maximum number of impressionOrder is the number of impressions we have from that timeline
-        maxlist.append(maxim) # adds the value to a list
-        minim = df2.impressionTime.values.min() # gets the start time of the timeline scrolling (before refresh or next timeline use)
-        dateslist.append(datetime.strptime(minim, '%Y-%m-%dT%H:%M:%S.%fZ'))
-    del df2
-    results = list(zip(dateslist, maxlist))
-    data = pd.DataFrame(results,
-                        columns=['time', 'impressions']
-                        )
-    data.set_index('time',
-                   inplace=True
-                   )
-    data.sort_index()
-    return data
 
-def setDatetimeIndex(df, what):
+'''Sets the index as datetime, can be either
+    by publicationTime or impressionTime'''
+
+def setDatetimeIndex(df, what='impression'):
     if what == 'publication':
         df = df.set_index(pd.DatetimeIndex(df['publicationTime']))
         return df
@@ -52,37 +26,58 @@ def setDatetimeIndex(df, what):
         df = df.set_index(pd.DatetimeIndex(df['impressionTime']))
         return df
     else:
-        print('what should be either publication or impression.')
+        print('\"what\" should be either \"publication\" or \"impression\".')
         raise ValueError
+
+
+'''Reduces datetime index from minimum values
+    of milliseconds to hours (1H), or days (1D)'''
 
 def setDatetimeIndexFloor(df, what):
     df.index = df.index.floor(what)
     return df
 
-def doAggregation(df):
-    # result = df[['user', 'timeline']].groupby(['user', 'timeline']).count()
+
+'''Returns an aggregated version of the Dataframe.
+    After setting the floor, we count all the values with the same index.'''
+
+def countImpressions(df):
     df = df.groupby(df.index).size()
     df = df.reset_index()
     df = df.set_index('impressionTime')
+    df.columns = ['impressions']
     return df
 
-# Filters Dataframe with start and end date. needs a datetime indexed dataframe
-# You can set start and end with formats "yyyy-mm-dd" or "yyyy-mm-dd hh:mm:ss"
+
+'''Filters Dataframe with start and end date. needs a datetime indexed dataframe
+    You can set start and end with formats "yyyy-mm-dd" or "yyyy-mm-dd hh:mm:ss"'''
 
 def setTimeframe(df, start, end):
-    data = df.loc[end:start]
+    df.sort_index(inplace=True, ascending=True)
+    data = df.loc[start:end]
     return data
+
+
+'''Saves dataframe to html table. Css styling
+    and names can be added in the future.'''
 
 def saveHtml(df, savename):
     # save
-    script_dir = os.path.dirname(os.path.dirname(__file__))  # <-- absolute dir the script is in
-    rel_path = "./local/" + savename + ".html"
-    strFile = os.path.join(script_dir, rel_path)
+    rel_path = savename + ".html"
+    strFile = os.path.join(config['path'], rel_path)
+    print('Saving to \'' + strFile + '\'')
+
     # make sure file is overwritten
     if os.path.isfile(strFile):
         print('File already exists: overwriting.')
         os.remove(strFile)
+
+    print('Done!')
     return df.to_html(strFile)
+
+
+'''Filters out the text column to get a Dataframe with impressions
+    that contain a specific keyword or string.'''
 
 def filterTexts(df, search):
     sb = df[df['texts'].str.contains(search)]
