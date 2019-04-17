b705f8c822b0add09f6a708c5017f8589232bff3
berli0z <40333748+berli0z@users.noreply.github.com>
2019-04-17 16:04:49 +0200

    Update app.py
    now saves data by default if called with the proper argument(s) as per issue #5

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,23 +1,43 @@
 from lib import API, tools, viz
 from lib.config import config
 
-savename = config['name']+'_impressions_'+config['start'][2:]+'_'+config['end'][2:]
+if config['impression-count'] == True:
+    savename = config['name'] + '_impressions_' + config['start'] + '_' + config['end']
+    df =  API.getDf(config['id'], 'summary', 30000, 0)
+    df = tools.setDatetimeIndex(df, 'impression')
+    df = tools.setDatetimeIndexFloor(df, '1H')
+    df = tools.setTimeframe(df, config['start'], config['end'])
+    df = tools.countImpressions(df)
+    if config['csv'] == True:
+        print('Saving CSV...')
+        df.to_csv(config['path']+savename+'.csv', index=True)
+        print('Saved to '+config['path']+savename+'.csv')
+    if config['json'] == True:
+        print('Saving JSON...')
+        df.to_json(config['path'] + savename + '.json')
+        print('Saved to ' + config['path'] + savename + '.json')
+    if config['html'] == True:
+        print('Saving HTML...')
+        tools.saveHtml(df, savename)
+    if config['png'] == True:
+        print('Saving PNG...')
+        viz.savePng(viz.formatDates(viz.impressionCount(df)), savename)
+else:
+    df =  API.getDf(config['id'], 'summary', 30000, 0)
+    savename = config['name'] + '_' + config['start'] + '_' + config['end']
+    if config['csv'] == True:
+        print('Saving CSV...')
+        df.to_csv(config['path'] + savename + '.csv', index=False)
+        print('Saved to ' + config['path'] + savename + '.csv')
 
-df =  API.getDf(config['id'], 'summary', 20000, 0)
-df = tools.setDatetimeIndex(df, 'impression')
-df = tools.setDatetimeIndexFloor(df, '1H')
-df = tools.setTimeframe(df, config['start'], config['end'])
-df = tools.countImpressions(df)
+    if config['json'] == True:
+        print('Saving JSON...')
+        df.to_json(config['path'] + savename + '.json')
+        print('Saved to ' + config['path'] + savename + '.json')
 
-if config['csv'] == True:
-    print('Saving CSV...')
-    df.to_csv(config['path']+savename+'.csv')
-    print('Saved to '+config['path']+savename+'.csv')
+    if config['html'] == True:
+        print('Saving HTML...')
+        tools.saveHtml(df, savename)
 
-if config['html'] == True:
-    print('Saving HTML...')
-    tools.saveHtml(df, savename)
-
-if config['png'] == True:
-    print('Creating png...')
-    viz.savePng(viz.formatDates(viz.impressionCount(df)), savename)
+    if config['png'] == True:
+        print('Cant save a PNG for the whole data yet, use --impression-count')
