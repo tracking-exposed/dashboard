e589a167e7cc8822a8de1673eb61cbd0be35652f
berli0z <40333748+berli0z@users.noreply.github.com>
2019-04-29 12:20:05 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,43 +1,58 @@
 from lib import API, tools, viz
 from lib.config import config
 
-if config['impression-count'] == True:
-    savename = config['name'] + '_impressions_' + config['start'] + '_' + config['end']
-    df =  API.getDf(config['token'], 'summary', 30000, 0)
-    df = tools.setDatetimeIndex(df, 'impression')
-    df = tools.setDatetimeIndexFloor(df, '1H')
-    df = tools.setTimeframe(df, config['start'], config['end'])
-    df = tools.countImpressions(df)
-    if config['csv'] == True:
-        print('Saving CSV...')
-        df.to_csv('/'+config['path']+savename+'.csv', index=True)
-        print('Saved to '+config['path']+savename+'.csv')
-    if config['json'] == True:
-        print('Saving JSON...')
-        df.to_json('/'+config['path'] + savename + '.json')
-        print('Saved to ' + config['path'] + savename + '.json')
-    if config['html'] == True:
-        print('Saving HTML...')
-        tools.saveHtml(df, savename)
-    if config['png'] == True:
-        print('Saving PNG...')
-        viz.savePng(viz.formatDates(viz.impressionCount(df)), savename)
-else:
-    df =  API.getDf(config['token'], 'summary', 30000, 0)
-    savename = config['name'] + '_' + config['start'] + '_' + config['end']
-    if config['csv'] == True:
-        print('Saving CSV...')
-        df.to_csv('/'+config['path'] + savename + '.csv', index=False)
-        print('Saved to ' + config['path'] + savename + '.csv')
+def main():
+    if config['impression-count'] == True:
+        
+         df =  API.getDf(config['token'], 'summary', 30000, 0)
+        savename = config['name'] + '_impressions_' + config['start'] + '_' + config['end']
+       
+        df = tools.setDatetimeIndex(df, 'impression')
+        df = tools.setDatetimeIndexFloor(df, '1H')
+        df = tools.setTimeframe(df, config['start'], config['end'])
+        df = tools.countImpressions(df)
+        
+        if config['csv'] == True:
+            print('Saving CSV...')
+            df.to_csv('/'+config['path']+savename+'.csv', index=True)
+            print('Saved to '+config['path']+savename+'.csv')
+            
+        if config['json'] == True:
+            print('Saving JSON...')
+            df.to_json('/'+config['path'] + savename + '.json')
+            print('Saved to ' + config['path'] + savename + '.json')
+            
+        if config['html'] == True:
+            print('Saving HTML...')
+            tools.saveHtml(df, savename)
+            
+        if config['png'] == True:
+            print('Saving PNG...')
+            viz.savePng(viz.formatDates(viz.impressionCount(df)), savename)
+            
+    else:
+        
+        df =  API.getDf(config['token'], 'summary', 30000, 0)
+        savename = config['name'] + '_' + config['start'] + '_' + config['end']
+        
+        if config['csv'] == True:
+            print('Saving CSV...')
+            df.to_csv('/'+config['path'] + savename + '.csv', index=False)
+            print('Saved to ' + config['path'] + savename + '.csv')
 
-    if config['json'] == True:
-        print('Saving JSON...')
-        df.to_json('/'+config['path'] + savename + '.json')
-        print('Saved to ' + config['path'] + savename + '.json')
+        if config['json'] == True:
+            print('Saving JSON...')
+            df.to_json('/'+config['path'] + savename + '.json')
+            print('Saved to ' + config['path'] + savename + '.json')
 
-    if config['html'] == True:
-        print('Saving HTML...')
-        tools.saveHtml(df, savename)
+        if config['html'] == True:
+            print('Saving HTML...')
+            tools.saveHtml(df, savename)
+            
+        if config['png'] == True:
+            print('Cant save a PNG for the whole data yet, use --impression-count')
+            
+    return
 
-    if config['png'] == True:
-        print('Cant save a PNG for the whole data yet, use --impression-count')
+if __name__ == "__main__":
+    main()
