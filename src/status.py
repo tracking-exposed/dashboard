7713069e0cadaad3f8c9a7358f280ce9cf667ee3
berli0z <40333748+berli0z@users.noreply.github.com>
2019-05-10 00:25:23 +0200

    adding function to check bot status in status.py

--- src/status.py
+++ src/status.py
@@ -1,23 +1,36 @@
 from lib import API, tools
 from lib.config import config
+from datetime import datetime, timezone, timedelta
+import pandas as pd
+import logging
 
-def main():
+logging.basicConfig(level=logging.WARNING, format='%(name)s - %(levelname)s - %(message)s')
 
-    if config['name'] != None:
-        path = config['path'] + '/' + config['name']
-    else:
-        path = config['path'] + '/' + config['token']
+if config['name'] != None:
+    path = config['path'] + '/' + config['name']
+else:
+    path = config['path'] + '/' + config['token']
 
-    df = API.getDf(config['token'], apiname='stats', count=config['amount'])
-    df= df[['startTime', 'impressionOrder']].groupby('startTime', as_index=True).max()
-    df = tools.setDatetimeIndexFloor(df, what=config['granularity']).groupby(df.index).sum()
+def checkBots():
+    df = API.getDf(config['token'], apiname='stats', count=1, skip=0)
+    # print(df)
 
-    if config['csv']:
-        df.to_csv(path+'_status.csv')
-    elif config['json']:
-        df.to_json(path+'_status.json')
-    else:
-        print(df.to_string())
+    # Check for parser errors
+    parsing_errors = len(df.loc[(df.summary.astype(str) == '[]') & -(df.htmlId.isnull())])
+    if parsing_errors > 0:
+        logging.warning('Encontered ' + str(parsing_errors) + ' parsing errors.')
+
+    # Check if the bot is down
+    delta = datetime.utcnow() - pd.to_datetime(df.startTime.max()).tz_localize(None)
+    if delta.seconds / 3600 > 1:
+        logging.critical('Warning, ' + config['name'] + ' is down since ' + str(delta))
+
+    # Check if bot is correctly getting impressions
+    max_impressionOrder = df.impressionOrder.max()
+    if max_impressionOrder < 35:
+        logging.error('Warning, ' + config['name'] + ' is having problems, only ' + str(max_impressionOrder) + ' impressions collected in the last hour.')
+
 
 if __name__ == "__main__":
-    main()
+    checkBots()
+
--- src/summary.py
+++ src/summary.py
@@ -19,3 +19,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''
+def getStatus():
+
+    if config['name'] != None:
+        path = config['path'] + '/' + config['name']
+    else:
+        path = config['path'] + '/' + config['token']
+
+    df = API.getDf(config['token'], apiname='stats', count=config['amount'])
+    # print(df)
+
+    df= df[['startTime', 'impressionOrder']].groupby('startTime', as_index=True).max()
+    df = tools.setDatetimeIndexFloor(df, what=config['granularity']).groupby(df.index).sum()
+
+    if config['csv']:
+        df.to_csv(path+'_status.csv')
+    elif config['json']:
+        df.to_json(path+'_status.json')
+    else:
+        print(df.to_string())
+        
+        '''
