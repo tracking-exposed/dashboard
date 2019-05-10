176ea5a2210f658177400730f2bb8a238c051939
berli0z <40333748+berli0z@users.noreply.github.com>
2019-05-10 19:30:58 +0200

    logging time as well

--- src/status.py
+++ src/status.py
@@ -1,15 +1,17 @@
 from lib import API, tools
 from lib.config import config
-from datetime import datetime, timezone, timedelta
+from datetime import datetime
 import pandas as pd
 import logging
 
-logging.basicConfig(level=logging.WARNING, format='%(name)s - %(levelname)s - %(message)s')
 
 if config['name'] != None:
     path = config['path'] + '/' + config['name']
 else:
     path = config['path'] + '/' + config['token']
+
+logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s', filename=path+'_status.log')
+
 
 def checkBots():
     df = API.getDf(config['token'], apiname='stats', count=1, skip=0)
@@ -18,7 +20,7 @@ def checkBots():
     # Check for parser errors
     parsing_errors = len(df.loc[(df.summary.astype(str) == '[]') & -(df.htmlId.isnull())])
     if parsing_errors > 0:
-        logging.warning('Encontered ' + str(parsing_errors) + ' parsing errors.')
+        logging.warning('Encountered ' + str(parsing_errors) + ' parsing errors.')
 
     # Check if the bot is down
     delta = datetime.utcnow() - pd.to_datetime(df.startTime.max()).tz_localize(None)
