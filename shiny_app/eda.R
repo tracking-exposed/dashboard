7b98e5fe05b4e5deaf0cdf606c9d05fe123fcdda
romatik <mikhail.balyasin@gmail.com>
2019-05-15 14:55:54 +0200

    Saving cached files into `shiny` folder.

--- shiny_app/eda.R
+++ shiny_app/eda.R
@@ -20,7 +20,7 @@ fb <- readr::read_csv("fbtrx_simple_csv/fbtrx_simple.csv", col_types = spec)
 #   dplyr::filter(impressionTime >= "2018-02-01",
 #                 impressionTime <  "2018-04-01")
 # saveRDS(fb, "subset.rds", compress = FALSE)
-# 
+#
 # fb <- readRDS("subset.rds")
 
 with_text <- fb %>%
@@ -30,11 +30,11 @@ with_text <- fb %>%
   dplyr::summarise(text = paste(unique(concatenatedText), collapse = ";")) %>%
   dplyr::ungroup() %>%
   dplyr::select(-postId)
- 
+
 stopwords <- jsonlite::fromJSON("stopwords-iso.json", simplifyDataFrame = TRUE) %>%
   purrr::map_dfr(~tibble::tibble(word = .x))
 
-with_text_tokenized <- tidytext::unnest_tokens(with_text, 
+with_text_tokenized <- tidytext::unnest_tokens(with_text,
                                                input = text, output = word) %>%
   dplyr::anti_join(dplyr::bind_rows(tidytext::stop_words,
                                     tibble::tibble(word = c("http", "https", "like", "doesn't",
@@ -44,7 +44,7 @@ with_text_tokenized <- tidytext::unnest_tokens(with_text,
 
 frequent_words <- with_text_tokenized %>%
   dplyr::count(word, impression_date) %>%
-  dplyr::filter(n > 200) %>% # keeps around 300 words 
+  dplyr::filter(n > 200) %>% # keeps around 300 words
   dplyr::distinct(word)
 
 frequency <- with_text_tokenized %>%
@@ -64,14 +64,14 @@ spiky_words <- frequency %>%
 
 frequency_filtered <- frequency %>%
   dplyr::filter(word %in% spiky_words$word)
-  
+
 p <- frequency_filtered %>%
   dplyr::group_by(word) %>%
   dplyr::mutate(lagged_n = lagged_n / max(lagged_n, na.rm = TRUE)) %>%
   dplyr::ungroup() %>%
   ggplot(aes(x = impression_date, y = lagged_n, color = word)) +
-  geom_line(alpha = 0.3) + 
-  geom_smooth(se = FALSE) + 
+  geom_line(alpha = 0.3) +
+  geom_smooth(se = FALSE) +
   coord_cartesian(ylim = c(-0.1, 0.1))
 
 
@@ -81,16 +81,16 @@ p <- frequency_filtered %>%
 lang_freq <- fb %>%
   dplyr::filter(!is.na(postId)) %>%
   dplyr::mutate(impression_date = as.Date(impressionTime)) %>%
-  dplyr::count(concatLanguage, impression_date) 
-saveRDS(lang_freq, "language_frequencies.rds", compress = FALSE)
+  dplyr::count(concatLanguage, impression_date)
+saveRDS(lang_freq, "shiny/language_frequencies.rds", compress = FALSE)
 
 ## most frequent word ------------------------------------------------------
 frequent_words <- with_text_tokenized %>%
   dplyr::count(word, impression_date) %>%
-  dplyr::filter(n > 200) %>% # keeps around 300 words 
+  dplyr::filter(n > 200) %>% # keeps around 300 words
   dplyr::distinct(word)
 
-saveRDS(frequent_words, "frequent_words.rds", compress = FALSE)
+saveRDS(frequent_words, "shiny/frequent_words.rds", compress = FALSE)
 
 
 ## precomputed frequency table ---------------------------------------------
@@ -103,7 +103,7 @@ frequency <- with_text_tokenized %>%
   dplyr::mutate(lagged_n = n - lag(n)) %>%
   dplyr::ungroup()
 
-saveRDS(frequency, "frequency_table.rds", compress = FALSE)
+saveRDS(frequency, "shiny/frequency_table.rds", compress = FALSE)
 
 ## subset for text of the post ---------------------------------------------
 set.seed(42)
@@ -113,5 +113,5 @@ subset <- fb %>%
   dplyr::mutate(impression_date = as.Date(impressionTime)) %>%
   dplyr::select(impression_date, concatenatedText, concatLanguage)
 
-saveRDS(subset, "subset_for_text.rds", compress = FALSE)
+saveRDS(subset, "shiny/subset_for_text.rds", compress = FALSE)
 
