a0f44dddcfbf0874873e60b2b8475c5181f1aa55
berli0z <40333748+berli0z@users.noreply.github.com>
2020-03-16 15:32:36 +0100

    updating stats tab on dashboard

--- bokeh_app/scripts/stats.py
+++ bokeh_app/scripts/stats.py
@@ -37,11 +37,28 @@ def stats_tab(posts_vs_ads, scrolling_time, source_count, post_count):
         p.legend.location = "top_center"
         return p
 
-    # def top_sources_pie(source_count):
-    #     return p
-    #
-    # def top_posts_pie(post_count):
-    #     return p
+    def top_sources_table(source_count):
+        # Columns of table
+        table_columns = [TableColumn(field='source', title='Source'),
+                         TableColumn(field='count', title='Count'),
+                         TableColumn(field='nature', title='Nature')
+                         ]
+
+        table = DataTable(source=source_count,
+                               columns=table_columns, width=1000)
+        return table
+
+    def top_posts_table(post_count):
+
+        table_columns = [TableColumn(field='source', title='Source'),
+                         TableColumn(field='texts', title='Title'),
+                         TableColumn(field='count', title='Count'),
+                         TableColumn(field='link', title='URL')
+                         ]
+
+        table = DataTable(source=post_count,
+                          columns=table_columns, width=1000)
+        return table
 
     def get_dataset(posts_vs_ads,
                     scrolling_time,
@@ -53,8 +70,8 @@ def stats_tab(posts_vs_ads, scrolling_time, source_count, post_count):
         posts_vs_ads_temp = posts_vs_ads_temp.set_index('user')[['organic', 'sponsored']].T.reset_index()
         posts_vs_ads_temp.columns = ['type', 'count']
         scrolling_time_temp = scrolling_time[scrolling_time['user'] == user]
-        source_count_temp = source_count[(source_count['user'] == user)].nlargest(ntop, 'count')
-        post_count_temp = post_count[(post_count['user'] == user)].nlargest(ntop, 'count')
+        source_count_temp = source_count[(source_count['user'] == user)].nlargest(int(ntop), 'count')
+        post_count_temp = post_count[(post_count['user'] == user)].nlargest(int(ntop), 'count')
         posts_vs_ads_source = ColumnDataSource(data=dict())
         posts_vs_ads_source.data = {
             'type': posts_vs_ads_temp.type,
@@ -100,18 +117,18 @@ def stats_tab(posts_vs_ads, scrolling_time, source_count, post_count):
         scrolling_time_source.data.update(scrolling_time_source_t.data)
         timespent.text = """User """ + str(list(scrolling_time_source.data["user"]))[2:-2] + """ spent an estimate of <b>""" + str(list(scrolling_time_source.data["time_spent"]))[2:-12] + """</b> scrolling facebook in the browser where
         facebook.tracking.exposed was installed."""
-        # source_count_source.data.update(source_count_source.data)
-        # post_count_source.data.update(post_count_source.data)
+        source_count_source.data.update(source_count_source_t.data)
+        post_count_source.data.update(post_count_source_t.data)
 
 
     # init vars
     name = source_count.user.iloc[0]
-    ntop = 5
+    ntop = 3.0
     names = source_count.user.unique()
 
     # init controls
     name_select = Select(value=name, title='User', options=sorted(names))
-    top_select = Slider(title="# of top sources/posts", value=0.0, start=1.0, end=25.0, step=1.0)
+    top_select = Slider(title="# of top sources/posts", value=3.0, start=3.0, end=20.0, step=1.0)
 
     # get data
     posts_vs_ads_source, scrolling_time_source, source_count_source, post_count_source = get_dataset(posts_vs_ads,
@@ -121,11 +138,12 @@ def stats_tab(posts_vs_ads, scrolling_time, source_count, post_count):
                                                                                                      name,
                                                                                                      ntop)
 
+
     # generate plots
     timespent = timespent_div(scrolling_time_source)
     postadspie = post_ads_bar(posts_vs_ads_source)
-    # topposts = top_posts_pie(source_count_source)
-    # topsources = top_sources_pie(post_count_source)
+    topposts = top_posts_table(post_count_source)
+    topsources = top_sources_table(source_count_source)
 
     # callbacks
     name_select.on_change('value', update)
@@ -136,8 +154,8 @@ def stats_tab(posts_vs_ads, scrolling_time, source_count, post_count):
     tab = Panel(child=column(controls,
                              row(timespent,
                                  postadspie),
-                             # row(topposts,
-                             #     topsources)
+                             topposts,
+                             topsources
                                   ),
                 title='Statistics')
     return tab
